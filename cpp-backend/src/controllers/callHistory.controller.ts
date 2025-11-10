1f7ade0880bd5f6e147397b5f21cdc7059400887
hemashi-garnara-prominentpixel <hemanshi.garnara@prominentpixel.com>
2025-11-10 15:22:41 +0530

    update code of controller for handle error

--- cpp-backend/src/controllers/callHistory.controller.ts
+++ cpp-backend/src/controllers/callHistory.controller.ts
@@ -4,20 +4,36 @@ import { CallHistory } from "../models/CallHistory.model";
 export const saveCall = async (req: Request, res: Response) => {
   try {
     const saved = await CallHistory.create(req.body);
-    res.status(201).json(saved);
+    res.status(201).json({
+      success: true,
+      message: saved,
+    });
   } catch (error) {
     console.log("Error - [saveCall in callHistory.controller.ts]", error);
-    res.status(500).json({ error });
+    if (error.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: "Contact ID already exists",
+      });
+    }
+
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
 export const getAllCalls = async (req: Request, res: Response) => {
   try {
     const calls = await CallHistory.find().sort({ createdAt: -1 });
-    res.status(200).json(calls);
+    res.status(200).json({
+      success: true,
+      message: calls,
+    });
   } catch (error) {
     console.log("Error - [getAllCalls in callHistory.controller.ts]", error);
-    res.status(500).json({ error });
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 
